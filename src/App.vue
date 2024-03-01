1b42adf10604c062339b8a3a0bbe732a4dd48a69
Mairo022 <mairo022@outlook.com>
2024-03-01 12:46:54 +0200

    Change theme

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import ProjectContent from "./components/project/ProjectContent.vue";
 </script>
 <template>
   <main class="flex flex-wrap gap-6 w-[90%] max-w-[1600px] max-h-fit mx-auto pt-[300px]">
-    <nav class="basis-[20%] bg-[#29292e] px-6 py-4 rounded-md">
+    <nav class="basis-[20%] bg-card px-6 py-4 rounded-md">
       <h1>Project Tracker</h1>
     </nav>
-    <section class="grow bg-[#29292e] px-6 py-4 rounded-md">
+    <section class="grow px-6 py-4 rounded-md bg-card">
       <ProjectHeader name="Driveclub"/>
       <ProjectContent title="To-do"/>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        linkhover: "hsl(var(--link-hover))",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
