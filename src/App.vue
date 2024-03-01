8888b6a11d9e3d8c453787b6e957d072d8136526
Mairo022 <mairo022@outlook.com>
2024-03-01 17:41:15 +0200

    Add menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import ProjectHeader from "./components/project/ProjectHeader.vue"
 import ProjectContent from "./components/project/ProjectContent.vue";
+import MenuContent from "./components/menu/MenuContent.vue";
+import MenuHeader from "./components/menu/MenuHeader.vue";
 </script>
 <template>
   <main class="flex flex-wrap gap-6 w-[90%] max-w-[1400px] max-h-fit mx-auto pt-[300px]">
-    <nav class="basis-[20%] bg-card px-6 py-4 rounded-md">
-      <h1>Project Tracker</h1>
+    <nav class="basis-[20%] bg-card px-6 py-4  border-2 rounded">
+      <MenuHeader/>
+      <MenuContent/>
     </nav>
     <section class="grow bg-card px-6 py-4 border-2 rounded">
       <ProjectHeader name="Driveclub"/>
---
+++ src/components/menu/MenuContent.vue
@@ -0,0 +1,61 @@
+<script setup>
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+</script>
+
+<template>
+  <div class="border-b pb-3 mb-3">
+    <Select>
+      <SelectTrigger>
+        <SelectValue placeholder="Select a project" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel class="border-b pb-3 mb-1">Projects</SelectLabel>
+          <SelectItem value="apple" class="cursor-pointer">
+            Apple
+          </SelectItem>
+          <SelectItem value="banana" class="cursor-pointer">
+            Banana
+          </SelectItem>
+          <SelectItem value="blueberry" class="cursor-pointer">
+            Blueberry
+          </SelectItem>
+          <SelectItem value="grapes" class="cursor-pointer">
+            Grapes
+          </SelectItem>
+          <SelectItem value="pineapple" class="cursor-pointer">
+            Pineapple
+          </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  </div>
+  <RadioGroup class="px-1" default-value="option-one">
+    <div class="flex items-center space-x-2 h-5">
+      <RadioGroupItem id="option-one" value="option-one" />
+      <Label class="cursor-pointer w-full" for="option-one">To-Do</Label>
+    </div>
+    <div class="flex items-center space-x-2 h-5">
+      <RadioGroupItem id="option-two" value="option-two" />
+      <Label class="cursor-pointer w-full" for="option-two">Bugs</Label>
+    </div>
+    <div class="flex items-center space-x-2 h-5">
+      <RadioGroupItem id="option-four" value="option-four" />
+      <Label class="cursor-pointer w-full" for="option-four">Ideas</Label>
+    </div>
+    <div class="flex items-center space-x-2 h-5">
+      <RadioGroupItem id="option-five" value="option-five" />
+      <Label class="cursor-pointer w-full" for="option-five">History</Label>
+    </div>
+  </RadioGroup>
+</template>
