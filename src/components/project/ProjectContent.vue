fc4966189c54448b9fa82d330206921a628fce6b
Mairo022 <mairo022@outlook.com>
2024-03-10 13:36:00 +0200

    Format time

--- src/components/project/ProjectContent.vue
+++ src/components/project/ProjectContent.vue
@@ -29,6 +29,26 @@ function translateView(view) {
   return viewValues.get(view)
 }
 
+function createdTime(inputTime) {
+  inputTime = new Date(inputTime)
+  const currentTime = new Date()
+  const difference = currentTime.getTime() - inputTime.getTime()
+
+  const seconds = difference / 1000
+  const minutes = seconds / 60
+  const hours = minutes / 60
+  const days = hours / 24
+  const weeks = days / 7
+
+  if (minutes < 1) return "1m"
+  if (minutes < 60) return Math.round(minutes) + "m"
+  if (hours < 24) return Math.round(hours) + "h"
+  if (days < 7) return Math.round(days) + "d"
+  if (weeks > 1) return Math.round(weeks) + "w"
+
+  return "NULL"
+}
+
 watch(props, () => {
   projectData.value = projectsData.get(props.projectId)[translateView(props.view)]
 }, { immediate: true })
@@ -53,7 +73,7 @@ watch(props, () => {
           <TableCell class="font-medium">
             {{item.title}}
           </TableCell>
-          <TableCell class="text-right text-primary">{{item.created}}</TableCell>
+          <TableCell class="text-right text-primary">{{createdTime(item.created)}}</TableCell>
           <TableCell class="text-right text-[#6ec075] cursor-pointer transition-colors hover:bg-muted/50">
             ✓
           </TableCell>
