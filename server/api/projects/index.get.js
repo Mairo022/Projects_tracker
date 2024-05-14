6da2514cc6cb8e11f79878d909e6839d11f29be9
Mairo022 <mairo022@outlook.com>
2024-05-14 10:34:58 +0300

    Order projects by name

--- server/api/projects/index.get.js
+++ server/api/projects/index.get.js
@@ -2,7 +2,7 @@ import db from "../../db";
 
 export default defineEventHandler(async () => {
     const res = await db.query({
-        text: 'SELECT id, name FROM projects',
+        text: 'SELECT id, name FROM projects ORDER BY name ASC',
         rowMode: 'object',
     })
     const projects = res.rows
